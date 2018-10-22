211c8df023078046fbbeb756835be7b5792c513d
Chandler Supple <36590015+chandlersupple@users.noreply.github.com>
2018-10-22 00:34:01 -0700

    Update dcgan.py

--- dcgan.py
+++ dcgan.py
@@ -14,6 +14,15 @@ def batch(batch_size, batch_iter):
     
     return batch_iarr
 
+def all_batches(batch_size, batches_in_epoch):
+    batches = []
+    for batch_iter in range (batches_in_epoch):
+        b = batch(batch_size, batch_iter)
+        b = 2 * (b - np.min(b)) / np.ptp(b) - 1
+        batches.append(b)
+    
+    return batches
+    
 def cnv_op(x, f, kernel, padding, stride= (2, 2)):
     o = tf.layers.conv2d(inputs= x, filters= f, kernel_size= kernel, strides= stride, padding= padding)
     
@@ -37,21 +46,22 @@ def generator(z, tr):
         
         resh_z = tf.reshape(l_one, [-1, 32, 32, 3])
         
-        conv_one = cnv_op_t(resh_z, 128, (5, 5), padding= 'same', stride= (1, 1))
+        conv_one = cnv_op_t(resh_z, 256, (4, 4), padding= 'same', stride= (1, 1))
         conv_one = tf.layers.batch_normalization(conv_one, training= t)
         conv_one = tf.nn.leaky_relu(conv_one)
         
-        conv_two = cnv_op_t(conv_one, 128, (5, 5), padding= 'same', stride= (1, 1))
+        conv_two = cnv_op_t(conv_one, 256, (4, 4), padding= 'same', stride= (1, 1))
         conv_two = tf.layers.batch_normalization(conv_two, training= t)
         conv_two = tf.nn.leaky_relu(conv_two)
         
-        conv_three = cnv_op_t(conv_one, 128, (5, 5), padding= 'same', stride= (1, 1))
+        conv_three = cnv_op_t(conv_one, 256, (4, 4), padding= 'same', stride= (1, 1))
         conv_three = tf.layers.batch_normalization(conv_three, training= t)
         conv_three = tf.nn.leaky_relu(conv_three)
         
-        conv_four = cnv_op_t(conv_three, 3, (5, 5), padding= 'same', stride= (1, 1))
+        conv_four = cnv_op_t(conv_three, 3, (4, 4), padding= 'same', stride= (1, 1))
+        act_conv_four = tf.nn.tanh(conv_four)
         
-        resh_conv_four = tf.reshape(conv_four, [-1, 32, 32, 3])
+        resh_conv_four = tf.reshape(act_conv_four, [-1, 32, 32, 3])
         
     return resh_conv_four
     
@@ -63,20 +73,19 @@ def discriminator(x, tr):
         else:
             t = False
         
-        conv_one = cnv_op(x, 128, (4, 4), padding= 'same')
+        conv_one = cnv_op(x, 64, (3, 3), padding= 'same')
         conv_one = tf.layers.batch_normalization(conv_one, training= t)
         conv_one = tf.nn.leaky_relu(conv_one)
         
-        conv_two = cnv_op(conv_one, 128, (4, 4), padding= 'same')
+        conv_two = cnv_op(conv_one, 64, (3, 3), padding= 'same')
         conv_two = tf.layers.batch_normalization(conv_two, training= t)
         conv_two = tf.nn.leaky_relu(conv_two)
         
-        conv_three = cnv_op(conv_two, 128, (4, 4), padding= 'valid')
+        conv_three = cnv_op(conv_two, 64, (3, 3), padding= 'valid')
         conv_three = tf.layers.batch_normalization(conv_three, training= t)
         conv_three = tf.nn.leaky_relu(conv_three)
         
-        conv_four = cnv_op(conv_three, 128, (2, 2), padding= 'valid', stride= (1, 1))
-        conv_four = tf.layers.batch_normalization(conv_four, training= t)
+        conv_four = cnv_op(conv_three, 64, (2, 2), padding= 'valid', stride= (1, 1))
         conv_four = tf.nn.leaky_relu(conv_four)
         
         l_one = tf.layers.dense(conv_four, 1)
@@ -106,24 +115,25 @@ t_vars = tf.trainable_variables()
 g_vars = [variable for variable in t_vars if 'g' in variable.name]
 d_vars = [variable for variable in t_vars if 'd' in variable.name]
 
-g_opt = tf.train.AdamOptimizer(0.01).minimize(g_loss, var_list= g_vars)
-d_opt = tf.train.AdamOptimizer(0.001).minimize(d_loss, var_list= d_vars)
+g_opt = tf.train.AdamOptimizer(0.001).minimize(g_loss, var_list= g_vars)
+d_opt = tf.train.GradientDescentOptimizer(0.001).minimize(d_loss, var_list= d_vars)
     
 sess.run(tf.global_variables_initializer())
     
 images = []
+batches = all_batches(batch_size, batches_in_epoch)
 
 for epoch_iter in range (epoch):
     for batch_iter in range (batches_in_epoch):
-        batch_xy = batch(batch_size, batch_iter)
+        batch_xy = batches[batch_iter]
         sess.run(g_opt, feed_dict= {z: np.random.normal(size= [batch_size, noise_num]), t: 1})
         sess.run(d_opt, feed_dict= {x: batch_xy, z: np.random.normal(size= [batch_size, noise_num]), t: 1})
         
-        if batch_iter % 5 == 0:
+        if batch_iter % 10 == 0:
             dl = sess.run(d_loss, feed_dict= {x: batch_xy, z: np.random.normal(size= [batch_size, noise_num]), t: 0})
             gl = sess.run(g_loss, feed_dict= {z: np.random.normal(size= [batch_size, noise_num]), t: 0})
             print('Epoch: %s, Batch %s / %s, D-loss: %s, G-loss: %s' %(epoch_iter, batch_iter, batches_in_epoch, dl, gl))
             
             g_img = sess.run(g, feed_dict= {z: np.random.normal(size= [1, noise_num]), t: 0})
-            g_img = image.array_to_img(np.reshape(g_img, [32, 32, 3]))
+            g_img = image.array_to_img(255 * np.reshape(g_img, [32, 32, 3]))
             images.append(g_img)
