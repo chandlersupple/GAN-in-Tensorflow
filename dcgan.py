5caa39d7433fcec5b4abdf67b1d873916c4f0219
Chandler Supple <36590015+chandlersupple@users.noreply.github.com>
2018-10-23 00:42:44 -0700

    Update dcgan.py

--- dcgan.py
+++ dcgan.py
@@ -1,3 +1,5 @@
+
+
 import tensorflow as tf
 import numpy as np
 from keras.preprocessing import image
@@ -46,19 +48,19 @@ def generator(z, tr):
         
         resh_z = tf.reshape(l_one, [-1, 32, 32, 3])
         
-        conv_one = cnv_op_t(resh_z, 256, (4, 4), padding= 'same', stride= (1, 1))
+        conv_one = cnv_op_t(resh_z, 256, (5, 5), padding= 'same', stride= (1, 1))
         conv_one = tf.layers.batch_normalization(conv_one, training= t)
         conv_one = tf.nn.leaky_relu(conv_one)
         
-        conv_two = cnv_op_t(conv_one, 256, (4, 4), padding= 'same', stride= (1, 1))
+        conv_two = cnv_op_t(conv_one, 256, (5, 5), padding= 'same', stride= (1, 1))
         conv_two = tf.layers.batch_normalization(conv_two, training= t)
         conv_two = tf.nn.leaky_relu(conv_two)
         
-        conv_three = cnv_op_t(conv_one, 256, (4, 4), padding= 'same', stride= (1, 1))
+        conv_three = cnv_op_t(conv_one, 256, (5, 5), padding= 'same', stride= (1, 1))
         conv_three = tf.layers.batch_normalization(conv_three, training= t)
         conv_three = tf.nn.leaky_relu(conv_three)
         
-        conv_four = cnv_op_t(conv_three, 3, (4, 4), padding= 'same', stride= (1, 1))
+        conv_four = cnv_op_t(conv_three, 3, (5, 5), padding= 'same', stride= (1, 1))
         act_conv_four = tf.nn.tanh(conv_four)
         
         resh_conv_four = tf.reshape(act_conv_four, [-1, 32, 32, 3])
@@ -73,19 +75,19 @@ def discriminator(x, tr):
         else:
             t = False
         
-        conv_one = cnv_op(x, 64, (3, 3), padding= 'same')
+        conv_one = cnv_op(x, 64, (4, 4), padding= 'same')
         conv_one = tf.layers.batch_normalization(conv_one, training= t)
         conv_one = tf.nn.leaky_relu(conv_one)
         
-        conv_two = cnv_op(conv_one, 64, (3, 3), padding= 'same')
+        conv_two = cnv_op(conv_one, 64, (4, 4), padding= 'same')
         conv_two = tf.layers.batch_normalization(conv_two, training= t)
         conv_two = tf.nn.leaky_relu(conv_two)
         
-        conv_three = cnv_op(conv_two, 64, (3, 3), padding= 'valid')
+        conv_three = cnv_op(conv_two, 64, (4, 4), padding= 'valid')
         conv_three = tf.layers.batch_normalization(conv_three, training= t)
         conv_three = tf.nn.leaky_relu(conv_three)
         
-        conv_four = cnv_op(conv_three, 64, (2, 2), padding= 'valid', stride= (1, 1))
+        conv_four = cnv_op(conv_three, 64, (3, 3), padding= 'valid', stride= (1, 1))
         conv_four = tf.nn.leaky_relu(conv_four)
         
         l_one = tf.layers.dense(conv_four, 1)
@@ -116,7 +118,7 @@ g_vars = [variable for variable in t_vars if 'g' in variable.name]
 d_vars = [variable for variable in t_vars if 'd' in variable.name]
 
 g_opt = tf.train.AdamOptimizer(0.001).minimize(g_loss, var_list= g_vars)
-d_opt = tf.train.GradientDescentOptimizer(0.001).minimize(d_loss, var_list= d_vars)
+d_opt = tf.train.AdamOptimizer(0.001).minimize(d_loss, var_list= d_vars)
     
 sess.run(tf.global_variables_initializer())
     
@@ -129,11 +131,11 @@ for epoch_iter in range (epoch):
         sess.run(g_opt, feed_dict= {z: np.random.normal(size= [batch_size, noise_num]), t: 1})
         sess.run(d_opt, feed_dict= {x: batch_xy, z: np.random.normal(size= [batch_size, noise_num]), t: 1})
         
-        if batch_iter % 10 == 0:
+        if batch_iter % 25 == 0:
             dl = sess.run(d_loss, feed_dict= {x: batch_xy, z: np.random.normal(size= [batch_size, noise_num]), t: 0})
             gl = sess.run(g_loss, feed_dict= {z: np.random.normal(size= [batch_size, noise_num]), t: 0})
             print('Epoch: %s, Batch %s / %s, D-loss: %s, G-loss: %s' %(epoch_iter, batch_iter, batches_in_epoch, dl, gl))
             
-            g_img = sess.run(g, feed_dict= {z: np.random.normal(size= [1, noise_num]), t: 0})
-            g_img = image.array_to_img(255 * np.reshape(g_img, [32, 32, 3]))
+            g_img = 255 * sess.run(g, feed_dict= {z: np.random.normal(size= [1, noise_num]), t: 0})
+            g_img = image.array_to_img(np.reshape(g_img, [32, 32, 3])) 
             images.append(g_img)
